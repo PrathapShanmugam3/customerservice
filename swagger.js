73f21eb39024d4a7f8d8ad985e8142eba3a856f6
prathapshanmugam5 <prathapshanmugam5@gmail.com>
2025-09-29 11:20:47 +0000

    swaggar changes

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT || 3000}`,
+        url: process.env.API_URL || 'http://localhost:3000',
       },
     ],
   },
