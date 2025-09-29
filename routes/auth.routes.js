0ff975afe63249f38bb7de0da0ff43d6027110fe
prathapshanmugam5 <prathapshanmugam5@gmail.com>
2025-09-29 11:24:37 +0000

    added swagger config

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,4 @@
-const verifySignUp = require("../middleware/verifySignUp");
+const { verifySignUp } = require("../middleware");
 const controller = require("../controllers/auth.controller");
 
 module.exports = function(app) {
@@ -10,6 +10,36 @@ module.exports = function(app) {
     next();
   });
 
+  /**
+   * @swagger
+   * /api/auth/signup:
+   *   post:
+   *     tags:
+   *       - Auth
+   *     summary: Register a new user
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               username:
+   *                 type: string
+   *               email:
+   *                 type: string
+   *               password:
+   *                 type: string
+   *               roles:
+   *                 type: array
+   *                 items:
+   *                   type: string
+   *     responses:
+   *       200:
+   *         description: User was registered successfully!
+   *       400:
+   *         description: Bad request
+   */
   app.post(
     "/api/auth/signup",
     [
@@ -19,7 +49,55 @@ module.exports = function(app) {
     controller.signup
   );
 
+  /**
+   * @swagger
+   * /api/auth/signin:
+   *   post:
+   *     tags:
+   *       - Auth
+   *     summary: Sign in
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               username:
+   *                 type: string
+   *               password:
+   *                 type: string
+   *     responses:
+   *       200:
+   *         description: User signed in successfully!
+   *       401:
+   *         description: Invalid password
+   *       404:
+   *         description: User not found
+   */
   app.post("/api/auth/signin", controller.signin);
 
+  /**
+   * @swagger
+   * /api/auth/refreshtoken:
+   *   post:
+   *     tags:
+   *       - Auth
+   *     summary: Refresh token
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               refreshToken:
+   *                 type: string
+   *     responses:
+   *       200:
+   *         description: Token was refreshed successfully!
+   *       403:
+   *         description: Refresh token is not in database!
+   */
   app.post("/api/auth/refreshtoken", controller.refreshToken);
 };
