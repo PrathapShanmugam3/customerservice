5ba9bab7959464dd71cb887d3e13bbac8a322865
prathapshanmugam5 <prathapshanmugam5@gmail.com>
2025-09-29 11:37:22 +0000

    id auto increment added

--- models/user.model.js
+++ models/user.model.js
@@ -2,7 +2,8 @@ module.exports = (sequelize, Sequelize) => {
   const User = sequelize.define("users", {
     id: {
       type: Sequelize.INTEGER,
-      primaryKey: true
+      primaryKey: true,
+      autoIncrement: true
     },
     username: {
       type: Sequelize.STRING
