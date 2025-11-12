dcc3c5cb847e6d88dfe89b66f7a6ba0956c1a6fc
prathapshanmugam5 <prathapshanmugam5@gmail.com>
2025-11-12 06:05:38 +0000

    user controller changed

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,11 +1,29 @@
 const db = require("../models");
 const User = db.user;
 
-// Example of a protected route
-exports.userBoard = (req, res) => {
+const allAccess = (req, res) => {
+  res.status(200).send("Public Content.");
+};
+
+const userBoard = (req, res) => {
   User.findByPk(req.userId).then(user => {
     res.status(200).send({
       message: `Welcome to the user board, ${user.name}!`
     });
   });
+};
+
+const adminBoard = (req, res) => {
+  res.status(200).send("Admin Content.");
+};
+
+const moderatorBoard = (req, res) => {
+  res.status(200).send("Moderator Content.");
+};
+
+module.exports = {
+  allAccess,
+  userBoard,
+  adminBoard,
+  moderatorBoard
 };
